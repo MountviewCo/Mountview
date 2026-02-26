b6987e83c0538470ae2b741151627de932069f28
Savana-Unana <svass@jlastudents.org>
2026-02-26 13:46:51 -0500

    Config.js update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 (function () {
     window.MOUNTVIEW_CONFIG = {
         // Google Apps Script Web App URL.
-        googleSheetsEndpoint: "https://script.google.com/macros/s/AKfycbxWpEeLvidFMqx1nyBLexfRuDgEXPWTu5orJLJYENC012H1s8s2OgHf2VzhUX3ggT-A/exec",
+        googleSheetsEndpoint: "https://script.google.com/macros/s/AKfycbz-BrZFX3oBcjVpjCsas13V-j6sgT0UiboW9TBTSaqsDrFNsqAXolZP53i1UTzX5qdE/exec",
         // Shared spreadsheet for this company workflow.
         spreadsheetId: "",
         // Company registry spreadsheet (company name, head account, invite link).
